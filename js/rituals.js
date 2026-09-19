0f6aef73de4860882b0a66a6113657ada449fb23
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 09:54:07 +0200

    So sei es: rituals loader, feld module, sw shell

--- js/rituals.js
+++ js/rituals.js
@@ -1,9 +1,15 @@
-import data from "./rituals-data.js";
-export const CAT_ORDER = data.CAT_ORDER;
-export const RITUALS = data.RITUALS;
+import soft from "./rituals-soft.js";
+import hard from "./rituals-hard.js";
+import feld from "./rituals-feld.js";
+
+export const CAT_ORDER = ["Alltag", "Schutz", "Energie", "Liebe", "Trennung", "Hard", "Feld"];
+
+export const RITUALS = [...soft, ...hard, ...feld];
+
 export function ritualById(id) {
   return RITUALS.find((r) => r.id === id);
 }
+
 export function ritualsBySide(side) {
   return RITUALS.filter((r) => r.side === side);
 }
