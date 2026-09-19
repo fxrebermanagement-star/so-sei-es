93db358dfb66b199fda3449b1f7ec25435900faf
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 09:52:51 +0200

    So sei es: rituals loader module

--- js/rituals.js
+++ js/rituals.js
@@ -1,1 +1,9 @@
-PLACEHOLDER
+import data from "./rituals-data.js";
+export const CAT_ORDER = data.CAT_ORDER;
+export const RITUALS = data.RITUALS;
+export function ritualById(id) {
+  return RITUALS.find((r) => r.id === id);
+}
+export function ritualsBySide(side) {
+  return RITUALS.filter((r) => r.side === side);
+}
