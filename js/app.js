ae6b1d64fc17ad50213ee135fc523c344bc01141
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 10:11:39 +0200

    App loader for raw-0..5 + SW 1.0.6

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ import r0 from "./raw-0.js";
 import r1 from "./raw-1.js";
 import r2 from "./raw-2.js";
 import r3 from "./raw-3.js";
+import r4 from "./raw-4.js";
+import r5 from "./raw-5.js";
 
-const code = r0 + r1 + r2 + r3;
+const code = r0 + r1 + r2 + r3 + r4 + r5;
 const run = new Function(
   "$", "$$", "esc", "uid", "fill", "fmtWhen", "hoursLeft",
   "loadChronik", "addChronik", "loadGabe", "saveGabe",
