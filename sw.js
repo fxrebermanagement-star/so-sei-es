d8c2e6d39e174dbc2efe3f44a5d3667acce64afe
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 09:46:31 +0200

    So sei es PWA: shell, styles, util, storage, manifest, sw

---
+++ sw.js
@@ -0,0 +1,55 @@
+/* So sei es service worker */
+const VERSION = "1.0.0";
+const CACHE = "so-sei-es-" + VERSION;
+const SHELL = [
+  "./",
+  "./index.html",
+  "./css/app.css",
+  "./js/util.js",
+  "./js/storage.js",
+  "./js/rituals.js",
+  "./js/app.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(async (c) => {
+      for (const url of SHELL) {
+        try { await c.add(url); } catch (err) { /* optional asset */ }
+      }
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k.startsWith("so-sei-es-") && k !== CACHE)
+          .map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then((cached) => {
+      const net = fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(req, copy));
+          return res;
+        })
+        .catch(() => cached);
+      return cached || net;
+    })
+  );
+});
