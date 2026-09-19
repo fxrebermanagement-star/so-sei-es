a7f2c8c36b0c630000b2ee95b215a686dbe79cd4
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 10:16:35 +0200

    Minified app loader (app-0..2) + SW 1.1.1

--- js/app.js
+++ js/app.js
@@ -4,26 +4,16 @@ import {
   hardLockActive, setHardLock, exportAll, importAll
 } from "./storage.js";
 import { RITUALS, CAT_ORDER, ritualById } from "./rituals.js";
-import r0 from "./raw-0.js";
-import r1 from "./raw-1.js";
-import r2 from "./raw-2.js";
-import r3 from "./raw-3.js";
-import r4 from "./raw-4.js";
-import r5 from "./raw-5.js";
-import r6 from "./raw-6.js";
-import r7 from "./raw-7.js";
-import r8 from "./raw-8.js";
-import r9 from "./raw-9.js";
-
-const code = r0 + r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9;
-const run = new Function(
+import p0 from "./app-0.js";
+import p1 from "./app-1.js";
+import p2 from "./app-2.js";
+new Function(
   "$", "$$", "esc", "uid", "fill", "fmtWhen", "hoursLeft",
   "loadChronik", "addChronik", "loadGabe", "saveGabe",
   "hardLockActive", "setHardLock", "exportAll", "importAll",
   "RITUALS", "CAT_ORDER", "ritualById",
-  code
-);
-run(
+  p0 + p1 + p2
+)(
   $, $$, esc, uid, fill, fmtWhen, hoursLeft,
   loadChronik, addChronik, loadGabe, saveGabe,
   hardLockActive, setHardLock, exportAll, importAll,
--- sw.js
+++ sw.js
@@ -1,21 +1,11 @@
 /* So sei es service worker */
-const VERSION = "1.0.7";
+const VERSION = "1.1.1";
 const CACHE = "so-sei-es-" + VERSION;
 const SHELL = [
   "./", "./index.html", "./css/app.css",
   "./js/util.js", "./js/storage.js", "./js/rituals.js",
   "./js/rituals-soft.js", "./js/rituals-hard.js", "./js/rituals-feld.js",
-  "./js/app.js",
-  "./js/raw-0.js",
-  "./js/raw-1.js",
-  "./js/raw-2.js",
-  "./js/raw-3.js",
-  "./js/raw-4.js",
-  "./js/raw-5.js",
-  "./js/raw-6.js",
-  "./js/raw-7.js",
-  "./js/raw-8.js",
-  "./js/raw-9.js",
+  "./js/app.js", "./js/app-0.js", "./js/app-1.js", "./js/app-2.js",
   "./manifest.webmanifest", "./assets/icon.svg"
 ];
 self.addEventListener("install", (e) => {
