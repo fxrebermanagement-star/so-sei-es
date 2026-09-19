8e2a57e02f48e0234fef9087d4c0e6ea2244a927
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 10:17:59 +0200

    App loader for p0_0..p3_2 + SW 1.2.0

--- js/app.js
+++ js/app.js
@@ -4,16 +4,24 @@ import {
   hardLockActive, setHardLock, exportAll, importAll
 } from "./storage.js";
 import { RITUALS, CAT_ORDER, ritualById } from "./rituals.js";
-import p0 from "./app-0.js";
-import p1 from "./app-1.js";
-import p2 from "./app-2.js";
-import p3 from "./app-3.js";
+import p0_0 from "./p0_0.js";
+import p0_1 from "./p0_1.js";
+import p0_2 from "./p0_2.js";
+import p1_0 from "./p1_0.js";
+import p1_1 from "./p1_1.js";
+import p1_2 from "./p1_2.js";
+import p2_0 from "./p2_0.js";
+import p2_1 from "./p2_1.js";
+import p2_2 from "./p2_2.js";
+import p3_0 from "./p3_0.js";
+import p3_1 from "./p3_1.js";
+import p3_2 from "./p3_2.js";
 new Function(
   "$", "$$", "esc", "uid", "fill", "fmtWhen", "hoursLeft",
   "loadChronik", "addChronik", "loadGabe", "saveGabe",
   "hardLockActive", "setHardLock", "exportAll", "importAll",
   "RITUALS", "CAT_ORDER", "ritualById",
-  p0 + p1 + p2 + p3
+  (p0_0+p0_1+p0_2)+(p1_0+p1_1+p1_2)+(p2_0+p2_1+p2_2)+(p3_0+p3_1+p3_2)
 )(
   $, $$, esc, uid, fill, fmtWhen, hoursLeft,
   loadChronik, addChronik, loadGabe, saveGabe,
--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 /* So sei es service worker */
-const VERSION = "1.1.2";
+const VERSION = "1.2.0";
 const CACHE = "so-sei-es-" + VERSION;
 const SHELL = [
   "./", "./index.html", "./css/app.css",
   "./js/util.js", "./js/storage.js", "./js/rituals.js",
   "./js/rituals-soft.js", "./js/rituals-hard.js", "./js/rituals-feld.js",
-  "./js/app.js", "./js/app-0.js", "./js/app-1.js", "./js/app-2.js", "./js/app-3.js",
+  "./js/app.js",
+  "./js/p0_0.js", "./js/p0_1.js", "./js/p0_2.js",
+  "./js/p1_0.js", "./js/p1_1.js", "./js/p1_2.js",
+  "./js/p2_0.js", "./js/p2_1.js", "./js/p2_2.js",
+  "./js/p3_0.js", "./js/p3_1.js", "./js/p3_2.js",
   "./manifest.webmanifest", "./assets/icon.svg"
 ];
 self.addEventListener("install", (e) => {
