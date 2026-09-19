292feee951f9b804ffd2f80402bb9007cd5b2135
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-19 10:13:30 +0200

    Loader for raw-0..9 + SW 1.0.7

--- js/app.js
+++ js/app.js
@@ -10,8 +10,12 @@ import r2 from "./raw-2.js";
 import r3 from "./raw-3.js";
 import r4 from "./raw-4.js";
 import r5 from "./raw-5.js";
+import r6 from "./raw-6.js";
+import r7 from "./raw-7.js";
+import r8 from "./raw-8.js";
+import r9 from "./raw-9.js";
 
-const code = r0 + r1 + r2 + r3 + r4 + r5;
+const code = r0 + r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9;
 const run = new Function(
   "$", "$$", "esc", "uid", "fill", "fmtWhen", "hoursLeft",
   "loadChronik", "addChronik", "loadGabe", "saveGabe",
--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
 /* So sei es service worker */
-const VERSION = "1.0.6";
+const VERSION = "1.0.7";
 const CACHE = "so-sei-es-" + VERSION;
 const SHELL = [
-  "./",
-  "./index.html",
-  "./css/app.css",
-  "./js/util.js",
-  "./js/storage.js",
-  "./js/rituals.js",
-  "./js/rituals-soft.js",
-  "./js/rituals-hard.js",
-  "./js/rituals-feld.js",
+  "./", "./index.html", "./css/app.css",
+  "./js/util.js", "./js/storage.js", "./js/rituals.js",
+  "./js/rituals-soft.js", "./js/rituals-hard.js", "./js/rituals-feld.js",
   "./js/app.js",
   "./js/raw-0.js",
   "./js/raw-1.js",
@@ -18,45 +12,31 @@ const SHELL = [
   "./js/raw-3.js",
   "./js/raw-4.js",
   "./js/raw-5.js",
-  "./manifest.webmanifest",
-  "./assets/icon.svg"
+  "./js/raw-6.js",
+  "./js/raw-7.js",
+  "./js/raw-8.js",
+  "./js/raw-9.js",
+  "./manifest.webmanifest", "./assets/icon.svg"
 ];
-
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(async (c) => {
-      for (const url of SHELL) {
-        try { await c.add(url); } catch (err) { /* optional asset */ }
-      }
-    }).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(async (c) => {
+    for (const url of SHELL) { try { await c.add(url); } catch (err) {} }
+  }).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((k) => k.startsWith("so-sei-es-") && k !== CACHE)
-          .map((k) => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.filter((k) => k.startsWith("so-sei-es-") && k !== CACHE).map((k) => caches.delete(k))
+  )).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", (e) => {
   const req = e.request;
   if (req.method !== "GET") return;
-  e.respondWith(
-    caches.match(req).then((cached) => {
-      const net = fetch(req)
-        .then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(req, copy));
-          return res;
-        })
-        .catch(() => cached);
-      return cached || net;
-    })
-  );
+  e.respondWith(caches.match(req).then((cached) => {
+    const net = fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy));
+      return res;
+    }).catch(() => cached);
+    return cached || net;
+  }));
 });
